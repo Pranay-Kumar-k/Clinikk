b7c9a00984df2e7b485763de8c7450a66e524a00
Pranay Kumar <pranaykumar31may1996@gmail.com>
2021-03-24 17:23:20 +0530

    added redux structure and designed schema for the mock api

--- clinikk-learning/src/App.js
+++ clinikk-learning/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
-import Sidebar from "./Components/Sidebar";
+import Sidebar from "./Components/Left/Sidebar";
+import RightComponent from "./Components/Right/RightComponent";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <RightComponent />
     </div>
   );
 }

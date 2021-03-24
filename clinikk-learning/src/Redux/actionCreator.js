3e16a7bd8dea455c88269f52dbd0b3015c858c1f
Pranay Kumar <pranaykumar31may1996@gmail.com>
2021-03-25 01:44:25 +0530

    Added Redux with actions, reducer and store

---
+++ clinikk-learning/src/Redux/actionCreator.js
@@ -0,0 +1,56 @@
+import { GET_ALL_COURSES_FAILURE, GET_ALL_COURSES_REQUEST, GET_ALL_COURSES_SUCCESS, GET_USER_FAILURE, GET_USER_REQUEST, GET_USER_SUCCESS } from "./actionType";
+import axios from "axios";
+
+export const getUserRequest = () => ({
+    type:GET_USER_REQUEST
+})
+
+export const getUserSuccess = (payload) => ({
+    type:GET_USER_SUCCESS,
+    payload
+})
+
+export const getUserFailure = () => ({
+    type:GET_USER_FAILURE
+})
+
+export const getUserData = (payload) => (dispatch) => {
+    dispatch(getUserRequest());
+    return axios({
+        method:'GET',
+        url:"https://aqueous-badlands-15880.herokuapp.com/users/pranay123"
+    })
+    .then((res) => {
+        console.log(res.data.pranay)
+        dispatch(getUserSuccess(res.data))
+    })
+    .catch((err) => {
+        console.log(err)
+        dispatch(getUserFailure())
+    })
+}
+
+export const getAllCoursesRequest = () => ({
+    type:GET_ALL_COURSES_REQUEST
+})
+
+export const getAllCoursesSuccess = (payload) => ({
+    type:GET_ALL_COURSES_SUCCESS
+})
+
+export const getAllCoursesFailure = () => ({
+    type:GET_ALL_COURSES_FAILURE
+})
+
+export const getAllCoursesData = () => (dispatch) => {
+    dispatch(getAllCoursesRequest())
+    return axios({
+        method:'GET',
+        url:"https://aqueous-badlands-15880.herokuapp.com/all_courses"
+    })
+    .then((res) => {
+        console.log(res.data)
+        dispatch(getAllCoursesSuccess(res.data))
+    })
+    .catch((err) => console.log(err))
+}
---
+++ clinikk-learning/src/Redux/reducer.js
@@ -0,0 +1,40 @@
+import { GET_ALL_COURSES_FAILURE, GET_ALL_COURSES_REQUEST, GET_ALL_COURSES_SUCCESS, GET_USER_FAILURE, GET_USER_REQUEST, GET_USER_SUCCESS } from "./actionType"
+
+
+const initState = {
+    user:[],
+    courses:[]
+}
+
+export const reducer = (state = initState,{type, payload}) => {
+    switch(type) {
+        case GET_USER_REQUEST:
+            return {
+                ...state
+            }
+        case GET_USER_SUCCESS:
+            return {
+                ...state,
+                user:payload
+            }
+        case GET_USER_FAILURE:
+            return {
+                ...state
+            }
+        case GET_ALL_COURSES_REQUEST:
+            return {
+                ...state
+            }
+        case GET_ALL_COURSES_SUCCESS:
+            return {
+                ...state,
+                courses:payload
+            }
+        case GET_ALL_COURSES_FAILURE:
+            return {
+                ...state
+            }
+        default:
+            return state
+    }
+}
